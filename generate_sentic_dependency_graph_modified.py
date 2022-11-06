78facf667fa5c6b52a1a1712c676507fd5c51ca3
yunbinmo <yunbinmo@gmail.com>
2022-11-06 22:25:20 +0800

    Update gitignore

--- generate_sentic_dependency_graph_modified.py
+++ generate_sentic_dependency_graph_modified.py
@@ -35,26 +35,50 @@ def dependency_adj_matrix(text, aspect, senticNet):
     #print(document)
     #print(senticNet)
     
+    # for token in document:
+    #     if str(token) in senticNet:
+    #         sentic = float(senticNet[str(token)])
+    #     else:
+    #         sentic = 0
+    #     if str(token) in aspect:
+    #         sentic += 1 * aspect_w
+    #     if token.i < seq_len:
+    #         matrix[token.i][token.i] = (1 * sentic + sentic) * sentic_w + 1
+    #         # https://spacy.io/docs/api/token
+    #         for child in token.children:
+    #             if str(child) in senticNet:
+    #                 child_sentic = float(senticNet[str(child)])
+    #             else:
+    #                 child_sentic = 0
+    #             if str(token) not in aspect and str(child) in aspect:
+    #                 child_sentic += 1 * aspect_w
+    #             if child.i < seq_len:
+    #                 matrix[token.i][child.i] = (1 * sentic + child_sentic) * sentic_w + 1
+    #                 matrix[child.i][token.i] = (1 * sentic + child_sentic) * sentic_w + 1
     for token in document:
         if str(token) in senticNet:
             sentic = float(senticNet[str(token)])
         else:
             sentic = 0
         if str(token) in aspect:
-            sentic += 1 * aspect_w
+            t = 1
+        else:
+            t = 0
         if token.i < seq_len:
-            matrix[token.i][token.i] = (1 * sentic + sentic) * sentic_w + 1
+            matrix[token.i][token.i] = 1 * ((sentic + sentic) + t + 1)
             # https://spacy.io/docs/api/token
             for child in token.children:
                 if str(child) in senticNet:
                     child_sentic = float(senticNet[str(child)])
                 else:
                     child_sentic = 0
-                if str(token) not in aspect and str(child) in aspect:
-                    child_sentic += 1 * aspect_w
+                if str(token) in aspect or str(child) in aspect:
+                    t = 1
+                else:
+                    t = 0
                 if child.i < seq_len:
-                    matrix[token.i][child.i] = (1 * sentic + child_sentic) * sentic_w + 1
-                    matrix[child.i][token.i] = (1 * sentic + child_sentic) * sentic_w + 1
+                    matrix[token.i][child.i] = 1 * ((sentic + child_sentic) + t + 1)
+                    matrix[child.i][token.i] = 1 * ((sentic + child_sentic) + t + 1)
     return matrix
 
 def process(filename):

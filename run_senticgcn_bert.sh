44c91bea917b6cd5c9239874344d5940658fa754
Cp-John <e0376984@u.nus.edu.sg>
2022-11-10 05:57:41 +0800

    Update sh

--- run_senticgcn_bert.sh
+++ run_senticgcn_bert.sh
@@ -1,1 +1,7 @@
-CUDA_VISIBLE_DEVICES=1 python train_bert.py --model_name senticgcn_bert --dataset rest14 --lr 2e-5 --seed 39 --batch_size 16
+rm -f output
+for s in 38 26 42 50 1001 12 122 4248 7 49
+do
+  echo "processing seed $s"
+  echo "processing seed $s" >> output
+  CUDA_VISIBLE_DEVICES=0 python train_bert.py --model_name senticgcn_bert --dataset rest14 --lr 2e-5 --seed "$s" --batch_size 16 >> output
+done

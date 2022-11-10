17399dc78fe153e2339581170daf555bc22e7e78
TomLBZ <libozhao142857@hotmail.com>
2022-11-11 02:09:16 +0800

    deleted unnecessary files

--- generate_graph.py
+++ generate_graph.py
@@ -60,8 +60,6 @@ def process(filename):
     fout.close() 
 
 if __name__ == '__main__':
-    process('./datasets/acl-14-short-data/train.raw')
-    process('./datasets/acl-14-short-data/test.raw')
     process('./datasets/semeval14/restaurant_train.raw')
     process('./datasets/semeval14/restaurant_test.raw')
     process('./datasets/semeval14/laptop_train.raw')

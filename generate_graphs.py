92b93554780a5031604cd8f2c253f86aaee2ad1c
TomLBZ <libozhao142857@hotmail.com>
2022-11-11 03:15:52 +0800

    combined gengraphs

--- generate_graphs.py
+++ generate_graphs.py
@@ -7,27 +7,22 @@ import pickle
 nlp = spacy.load('en_core_web_sm')
 
 def dependency_adj_matrix(text):
-    # https://spacy.io/docs/usage/processing-text
     document = nlp(text)
     seq_len = len(text.split())
     matrix = np.zeros((seq_len, seq_len)).astype('float32')
-    
     for token in document:
         if token.i < seq_len:
             matrix[token.i][token.i] = 1
-            # https://spacy.io/docs/api/token
             for child in token.children:
                 if child.i < seq_len:
                     matrix[token.i][child.i] = 1
                     matrix[child.i][token.i] = 1
-
     return matrix
 
 def sentic_adj_matrix(text, aspect, senticNet):
     word_list = text.split()
     seq_len = len(word_list)
     matrix = np.zeros((seq_len, seq_len)).astype('float32')
-    
     for i in range(seq_len):
         word = word_list[i]
         if word in senticNet:
@@ -42,7 +37,27 @@ def sentic_adj_matrix(text, aspect, senticNet):
     for i in range(seq_len):
         if matrix[i][i] == 0:
             matrix[i][i] = 1
+    return matrix
 
+def sent_dep_adj_matrix(text, aspect, senticNet):
+    document = nlp(text)
+    seq_len = len(text.split())
+    matrix = np.zeros((seq_len, seq_len)).astype('float32')
+    for token in document:
+        if str(token) in senticNet:
+            sentic = float(senticNet[str(token)]) + 1
+        else:
+            sentic = 0
+        if str(token) in aspect:
+            sentic += 1
+        if token.i < seq_len:
+            matrix[token.i][token.i] = 1 * sentic
+            for child in token.children:
+                if str(child) in aspect:
+                    sentic += 1
+                if child.i < seq_len:
+                    matrix[token.i][child.i] = 1 * sentic
+                    matrix[child.i][token.i] = 1 * sentic
     return matrix
 
 def load_sentic_word():
@@ -67,18 +82,23 @@ def process(filename):
         lines = fin.readlines()
     idx2dgraph = {}
     idx2sgraph = {}
+    idx2sdgraph = {}
     fgout = open(filename+'.graph', 'wb')
     fsout = open(filename+'.sentic', 'wb')
+    fsdout = open(filename+'.graph_sdat', 'wb')
     for i in range(0, len(lines), 3):
         text_left, _, text_right = [s.lower().strip() for s in lines[i].partition("$T$")]
         aspect = lines[i + 1].lower().strip()
         idx2dgraph[i] = dependency_adj_matrix(text_left+' '+aspect+' '+text_right)
         idx2sgraph[i] = sentic_adj_matrix(text_left+' '+aspect+' '+text_right, aspect, senticNet)
+        idx2sdgraph[i] = sent_dep_adj_matrix(text_left+' '+aspect+' '+text_right, aspect, senticNet)
     pickle.dump(idx2dgraph, fgout)  
     pickle.dump(idx2sgraph, fsout)
+    pickle.dump(idx2sdgraph, fsdout)
     print('done !!!', filename)      
-    fgout.close() 
-    fsout.close() 
+    fgout.close()
+    fsout.close()
+    fsdout.close()
 
 if __name__ == '__main__':
     process('./datasets/semeval14/restaurant_train.raw')

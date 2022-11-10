15bbbb83377e5d6157eaa2846a50fd10a05e9b23
TomLBZ <libozhao142857@hotmail.com>
2022-11-11 03:07:48 +0800

    removed extra files

---
+++ generate_graphs.py
@@ -0,0 +1,91 @@
+# -*- coding: utf-8 -*-
+
+import numpy as np
+import spacy
+import pickle
+
+nlp = spacy.load('en_core_web_sm')
+
+def dependency_adj_matrix(text):
+    # https://spacy.io/docs/usage/processing-text
+    document = nlp(text)
+    seq_len = len(text.split())
+    matrix = np.zeros((seq_len, seq_len)).astype('float32')
+    
+    for token in document:
+        if token.i < seq_len:
+            matrix[token.i][token.i] = 1
+            # https://spacy.io/docs/api/token
+            for child in token.children:
+                if child.i < seq_len:
+                    matrix[token.i][child.i] = 1
+                    matrix[child.i][token.i] = 1
+
+    return matrix
+
+def sentic_adj_matrix(text, aspect, senticNet):
+    word_list = text.split()
+    seq_len = len(word_list)
+    matrix = np.zeros((seq_len, seq_len)).astype('float32')
+    
+    for i in range(seq_len):
+        word = word_list[i]
+        if word in senticNet:
+            sentic = float(senticNet[word]) + 1.0
+        else:
+            sentic = 0
+        if word in aspect:
+            sentic += 1.0
+        for j in range(seq_len):
+            matrix[i][j] += sentic
+            matrix[j][i] += sentic
+    for i in range(seq_len):
+        if matrix[i][i] == 0:
+            matrix[i][i] = 1
+
+    return matrix
+
+def load_sentic_word():
+    """
+    load senticNet
+    """
+    path = './senticNet/senticnet_word.txt'
+    senticNet = {}
+    fp = open(path, 'r')
+    for line in fp:
+        line = line.strip()
+        if not line:
+            continue
+        word, sentic = line.split('\t')
+        senticNet[word] = sentic
+    fp.close()
+    return senticNet
+
+def process(filename):
+    senticNet = load_sentic_word()
+    with open(filename, 'r', encoding='utf-8', newline='\n', errors='ignore') as fin:
+        lines = fin.readlines()
+    idx2dgraph = {}
+    idx2sgraph = {}
+    fgout = open(filename+'.graph', 'wb')
+    fsout = open(filename+'.sentic', 'wb')
+    for i in range(0, len(lines), 3):
+        text_left, _, text_right = [s.lower().strip() for s in lines[i].partition("$T$")]
+        aspect = lines[i + 1].lower().strip()
+        idx2dgraph[i] = dependency_adj_matrix(text_left+' '+aspect+' '+text_right)
+        idx2sgraph[i] = sentic_adj_matrix(text_left+' '+aspect+' '+text_right, aspect, senticNet)
+    pickle.dump(idx2dgraph, fgout)  
+    pickle.dump(idx2sgraph, fsout)
+    print('done !!!', filename)      
+    fgout.close() 
+    fsout.close() 
+
+if __name__ == '__main__':
+    process('./datasets/semeval14/restaurant_train.raw')
+    process('./datasets/semeval14/restaurant_test.raw')
+    process('./datasets/semeval14/laptop_train.raw')
+    process('./datasets/semeval14/laptop_test.raw')
+    process('./datasets/semeval15/restaurant_train.raw')
+    process('./datasets/semeval15/restaurant_test.raw')
+    process('./datasets/semeval16/restaurant_train.raw')
+    process('./datasets/semeval16/restaurant_test.raw')
